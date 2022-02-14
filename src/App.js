32352975fddd1336fcb90240179ba15149f81323
Lyes <ramdanelyes96@gmail.com>
2022-02-14 17:14:44 +0100

    debuging

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Movies from './components/Movie';
-import axios from "axios";
 import { useState,useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Movies from './components/Movie';
+import GetPopular from './services/PopularMovies';
 
 function App() {
 
+
   const [movies, setMovies] = useState([]);
 
-  
+
+  GetPopular()
   useEffect(() => {
     console.log("useEffect");
-    GetPopular();
   }, []);
 
-  const GetPopular = async () => {
-    const data = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=8433cf99d057dc015d472aaa543637c0&language=en-US&page=1")
-    const movies = await data.data.results;
 
-    setMovies(movies);
-  }
   
 
   return (
