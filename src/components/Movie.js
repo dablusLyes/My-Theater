50e0a82baac39975aa0b145cc19e4d0256ac8587
Lyes <lyes.dabla@gmail.com>
2022-02-14 10:13:00 +0100

    refactoring

---
+++ src/components/Movie.js
@@ -0,0 +1,22 @@
+
+const Movie = ({ movie }) => {
+    let note_color;
+    if (movie.vote_average > 5) {
+        note_color = 'green'
+    } else {
+        note_color = 'red'
+    }
+
+    return (
+        <div className="movie">
+            <h2 className="movie-title">{movie.title}</h2>
+            <div className="img-info">
+                <img className="movie-image" src={'https://image.tmdb.org/t/p/w500/' + movie.backdrop_path} alt="" />
+                <p className="note"><span className={note_color + '_note'}>{movie.vote_average}</span>/10</p>
+                <p className="movie-description">{movie.overview}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Movie; 
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className="nav-bar">
+            <h1 className="app-title">My Theater</h1>
+            <div className="nav-buttons">
+                <a href="">popular movies</a>
+                <a href="">Releases</a>
+            </div>
+        </div>
+    )
+}
+
+
+export default Navbar;
