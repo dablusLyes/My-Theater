3cd40bbc74d41c327e151420afd652faa2f73372
Lyes <ramdanelyes96@gmail.com>
2022-02-14 17:14:52 +0100

    debuging

---
+++ src/services/PopularMovies.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+  
+
+const GetPopular = async () => {
+    const data = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=8433cf99d057dc015d472aaa543637c0&language=en-US&page=1")
+    const movies = await data.data.results;
+    
+    console.log(movies)
+
+    return movies;
+  }
+  
+  export default GetPopular;
